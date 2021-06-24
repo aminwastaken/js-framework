0eabe26ad00f63597f0d23534e0b8bbaa0584ccb
amin <aminwastaken@gmail.com>
2021-06-24 18:50:23 +0200

    Basic setup with a render function

---
+++ App.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+var a = parent.document.createElement("p");
+a.innerHTML = "hello";
+
+const root = document.getElementById("root");
+root.appendChild(a);
---
+++ Component.js
@@ -0,0 +1,30 @@
+const generateNodeElements = (obj) => {
+  const elementToRender = document.createElement(obj.Type);
+  if (obj.children) {
+    for (let child of obj.children) {
+      childToRender =
+        typeof child === "string"
+          ? document.createTextNode(child) // if the child is a string, a text node is created
+          : generateNodeElements(child); // the generateNodeElements function gets called for each child that isn't a string
+      elementToRender.appendChild(childToRender); // all children are added inside the parent element
+    }
+  }
+
+  // getting all the event listeners from the input and implementing them in the element to render
+  if (obj.attribute) {
+    for (let attribute in obj.attributes) {
+      if (attribute.startsWith("on")) {
+        elementToRender.addEventListener(
+          attribute.replace(/^on/, ""),
+          obj.attributes[attribute]
+        );
+      }
+    }
+  }
+
+  return element;
+};
+
+const Component = {};
+
+export default Component;
