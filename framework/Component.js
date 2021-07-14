8a0b08719a5149880190167bec890db2f0c4955d
amin <aminwastaken@gmail.com>
2021-07-14 18:04:13 +0200

    Added should update func + Assembled all the framework elements inside the same class

--- framework/Component.js
+++ framework/Component.js
@@ -1,10 +1,16 @@
 import createElement from "./functions/createElement.js";
 import render from "./functions/render.js";
+import shouldUpdate from "./functions/shouldUpdate.js";
 
 //this object contains all of the framework tools
-const Component = {
-  render: render,
-  createElement: createElement,
+
+const Component = function () {};
+Component.prototype.render = render;
+Component.prototype.createElement = createElement;
+Component.prototype.shouldUpdate = shouldUpdate;
+Component.prototype.display = undefined;
+Component.prototype.constructor = function () {
+  return this.render(this.buildComponenent());
 };
 
 export default Component;
